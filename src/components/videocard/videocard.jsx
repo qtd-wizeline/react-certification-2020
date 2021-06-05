41bdd29363628af7e378fe46f1c29faeb697f324
Juan Pablo <juanpcortesg@gmail.com>
2021-06-04 23:33:29 -0500

    added basic style

--- src/components/videocard/videocard.jsx
+++ src/components/videocard/videocard.jsx
@@ -1,15 +1,32 @@
 import React from "react"
+import styled from "styled-components"
 
+const VideoWrapper = styled.div`
+  border-radius: 5px;
+  margin: auto;
+  height: 480px;
+  padding: 0 0 50 0;
+  max-width: 720px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+`;
+
+const Title = styled.div`
+  font-size: 2em;
+  margin: auto;
+  font-color:black;
+  `;
 
 function VideoCard({thumbnail,title}){
     return(
-        <div className = "card">
-            <h2>
+        <VideoWrapper>
+            <Title>
                 {title}
-            </h2>
+            </Title>
             <img src={thumbnail}></img>
             
-        </div>
+        </VideoWrapper>
 
     )
 }
