5344722f8539955cb7d4663e66e53b506aca6dc8
David Lucero <dvlucero85@gmail.com>
2021-06-13 00:27:01 +0800

    deliv-1 using styled-components on the header

--- src/components/Body/Body.component.jsx
+++ src/components/Body/Body.component.jsx
@@ -1,29 +1,19 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-
 // import { useFortune } from '../../utils/hooks/useFortune';
 import './Body.styles.css';
 import Layout from '../Layout';
 import HomePage from '../../pages/Home';
 import NotFound from '../../pages/NotFound';
 
-
-function Body(item) {
-  const text = 'Demo App! Welcome!';
-
+function Body() {
   return (
     <Layout>
       <Switch>
         <Route exact path="/">
           <HomePage />
         </Route>
-        {/* <Route exact path="/login">
-          <LoginPage />
-        </Route>
-        <Private exact path="/secret">
-          <SecretPage />
-        </Private> */}
         <Route path="*">
           <NotFound />
         </Route>
--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,20 +1,58 @@
 import React from 'react';
+import styled from 'styled-components';
 
-// import { useFortune } from '../../utils/hooks/useFortune';
-import './Header.styles.css';
+const ButtonLeft = styled.button`
+  left: 0%;
+  float: left;
+  /* Adapt the colors based on primary prop */
+  background: #ede9e8;
+  color: gray;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid black;
+  border-radius: 3px;
+`;
 
-function Header() {
-  const text = 'Demo App! Welcome!';
+const ButtonRight = styled.button`
+  right: 100%;
+  float: right;
+  /* Adapt the colors based on primary prop */
+  background: white;
+  color: black;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid black;
+  border-radius: 3px;
+`;
 
+const SearchField = styled.input`
+  padding: 12px 15px;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  font-size: 18px;
+  min-width: 400px;
+`;
+
+const Header = styled.header`
+  color: white;
+  margin: 0;
+  height: 7em;
+  background-color: black;
+`;
+
+
+function Component() {
   return (
-    <header className="header">
+    <Header>
       <div>
-        <button className="button-left">Navigation</button>
-        <input disabled />
-        <button className="button-right">Login</button>
+        <ButtonLeft>Navigation</ButtonLeft>
+        <SearchField disabled />
+        <ButtonRight>Login</ButtonRight>
       </div>
-    </header>
+    </Header>
   );
 }
 
-export default Header;
+export default Component;
