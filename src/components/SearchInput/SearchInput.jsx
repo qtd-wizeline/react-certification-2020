42b5689d42b4a3f5ef67dc3a6f14b22dc2635e23
immortaldog_ <laminh711@gmail.com>
2021-06-30 23:19:30 +0700

    add eslint ignore fix for deploy

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import React, { useContext, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
